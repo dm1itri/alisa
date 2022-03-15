7c17d19de61c3ab5e79a00443fcfc69a55cd00f2
dm1itri <dimma-18@yandex.ru>
2022-03-15 19:10:37 +0300

    start

--- flask_app.py
+++ flask_app.py
@@ -88,12 +88,12 @@ def handle_dialog(req, res):
     # Если он написал 'ладно', 'куплю', 'покупаю', 'хорошо',
     # то мы считаем, что пользователь согласился.
     # Подумайте, всё ли в этом фрагменте написано "красиво"?
-    if req['request']['original_utterance'].lower() in [
+    if True in [w in req['request']['original_utterance'].lower() for w in [
         'ладно',
         'куплю',
         'покупаю',
         'хорошо'
-    ]:
+    ]]:
         # Пользователь согласился, прощаемся.
         res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
         res['response']['end_session'] = True
